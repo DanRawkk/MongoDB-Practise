111fa7433f9bf2476960c99084456fcd142f3da9
Unknown <becario.sergio@gmail.com>
2017-03-27 00:37:43 -0500

    Se agrego el Post new todos y el get all todos

--- server/server.js
+++ server/server.js
@@ -11,11 +11,38 @@ const app = express();
 de text a json. */
 app.use(bodyParser.json());
 
+
+/******************************************************************************/
 /* Creamos la ruta para crear nuevos todos */
 app.post('/todos', (req, res) => {
-  console.log(req.body);
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (err) => {
+    res.status(400).send(err);
+    /* En caso de error podemos solo enviar el error "res.send(err)" pero Tambien
+    podemos elegir cual estatus de error, enviar, en este caso elejimos el 400 */
+  });
+});
+/******************************************************************************/
+/* Con este pediremos todos los posts */
+app.get('/todos', (req,res) => {
+  Todo.find().then((todos) => {
+    /* La respuesta nos mandara un array de los todos. y auqnue podriamos solo
+    resivirlo y enviarlo asi "res.send(todos)" no es la mejor forma porque si
+    queremos agregarle mas configuraciones como el status code y esas cosas, no
+    se podra, asi qe mejor lomandamos en un objeto {todos: todos} y asi si lo
+    deseamos podemos ponerle mas propiedades al objeto como su estatus y mas. */
+    res.send({todos});
+  }, (err) => {
+    res.status(400).send(err);
+  });
 });
 
+/******************************************************************************/
 app.listen(3000, () => {
   console.log('Listening on port: 3000');
 });
