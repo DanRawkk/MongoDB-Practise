df4feb2de3b6a35c3f00b9bcf44ea95988794984
Sergio <becario.sergio@gmail.com>
2017-04-02 21:42:45 -0500

    Ya se puede actualisar los todos si fueron completados o su texto

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -68,7 +69,69 @@ app.get('/todos/:id', (req, res) => {
       return res.status(400).send();
     });
 });
+/******************************************************************************/
+/* Con patch lo que aremos sera hacer updates a nuestros post. */
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  /* En el caso de hacer un 'update' queremos que el usuario solo pueda acceder
+  a las caracteristicas que queremos del objeto y no a todos como en este caso
+  al 'id' o a la fecha de completado. Entonces usamos '_.pick()' que lo que hace
+  es que le pasamos un objeto completo como primer argumento y de segundo
+  argumento pasamos un array de las propiedades que le queremos sacar y nos
+  retorna un objeto con esas unicaspropiedades, en este caso solo queremos que
+  pueda modificar el texto y si a sido completado.
+  src: https://lodash.com/docs/4.17.4#pick */
+  const body = _.pick(req.body, ['text', 'completed']);
 
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  /* En caso de que el 'todo' no se modifique el texto y solo se modifique si se
+  completó checaremos si nuestro la propiedad 'completed' es un boolean y si es
+  true agregaremos una nueva propiedad al objeto que sera la fecha de completado.
+  */
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();// Hora que fue completado
+  } else {//En caso de ponerlo como no completado
+    body.completed = false;
+    body.completedAt = null;// Se borra la hora de completado
+  }
+  /* El '.findByIdAndUpdate(id, update, options, callback )' recive 4 parametros
+  aunque minimo puede recibir "id y update".
+
+    Como ya sabemos id es el id del documento a modificar.
+
+    En 'update' se le pasan los campos que seran modificados con el contenido
+  que sera modificado con un objeto:
+  {$set: {key: valueModified, key2: valueModified}} usando '$set' para decir que
+  esas son las propiedadesque seran modificadas, en este caso pasaremos el objeto
+  'body'.
+
+    En 'options' Tambien se le pasa un objeto con las sigientes opciones:
+  -new: bool - true to return the modified document rather than the original. defaults to false
+  -upsert: bool - creates the object if it doesn't exist. defaults to false.
+  -runValidators: if true, runs update validators on this command. Update validators validate the update operation against the model's schema.
+  -setDefaultsOnInsert: if this and upsert are true, mongoose will apply the defaults specified in the model's schema if a new document is created. This option only works on MongoDB >= 2.4 because it relies on MongoDB's $setOnInsert operator.
+  -sort: if multiple docs are found by the conditions, sets the sort order to choose which doc to update
+  -select: sets the document fields to return
+
+    Y en el 'callback' lo que queremos que aga al finalisar.
+
+  src: http://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate */
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true, runValidators: true})
+    .then((todo) => {
+      if(!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({todo});
+    }).catch((err) => {
+      res.status(400).send();
+    });
+});
+
+/******************************************************************************/
 app.delete('/todos/:id', (req, res) => {
   const id = req.params.id;
 
